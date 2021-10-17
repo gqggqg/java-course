c8cf8640b8383ea2d9c68cbfc33dde1ce4f4c85d
gqggqg <71404862+gqggqg@users.noreply.github.com>
2021-10-17 22:45:16 +0200

    guice project creating

---
+++ hw_02_guice/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    java
+}
+
+group = "com.gqggqg"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.inject:guice:5.0.1")
+    implementation("org.jetbrains:annotations:13.0")
+}
---
+++ hw_02_guice/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.jetbrains.annotations.NotNull;
+
+public class Main {
+
+    public static void main(@NotNull String[] args) {
+
+    }
+}
