fc3b1534ca444e343aa7f5a2fd9c2f81d1dbfebe
gqggqg <71404862+gqggqg@users.noreply.github.com>
2021-10-18 23:01:55 +0200

    fix guice bind configuration

--- hw_02_guice/src/main/java/AppModule.java
+++ hw_02_guice/src/main/java/AppModule.java
@@ -4,7 +4,7 @@ public final class AppModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(ILogger.class).to(ConsoleLogger.class);
+        bind(ILogger.class).to(CombinedLogger.class);
         bind(ILogger.class).annotatedWith(FileLog.class).to(FileLogger.class);
         bind(ILogger.class).annotatedWith(ConsoleLog.class).to(ConsoleLogger.class);
     }
