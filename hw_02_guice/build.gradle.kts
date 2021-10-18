8593ce0117710377ef4ef5460e90e1dded9b9687
gqggqg <71404862+gqggqg@users.noreply.github.com>
2021-10-18 23:36:04 +0200

    deleting unused dependency

--- hw_02_guice/build.gradle.kts
+++ hw_02_guice/build.gradle.kts
@@ -13,7 +13,6 @@ repositories {
 dependencies {
     implementation("com.google.inject:guice:5.0.1")
     implementation("org.jetbrains:annotations:22.0.0")
-    compileOnly("org.projectlombok:lombok:1.18.22")
 }
 
 application {
