8628b3b4ac337d4bcc557d21d78ce00a4ce52140
gqggqg <71404862+gqggqg@users.noreply.github.com>
2021-10-18 23:35:28 +0200

    adding application plugin

--- hw_02_guice/build.gradle.kts
+++ hw_02_guice/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    application
 }
 
 group = "com.gqggqg"
@@ -13,4 +14,12 @@ dependencies {
     implementation("com.google.inject:guice:5.0.1")
     implementation("org.jetbrains:annotations:22.0.0")
     compileOnly("org.projectlombok:lombok:1.18.22")
+}
+
+application {
+    mainClass.set("guice.Main")
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
 }
