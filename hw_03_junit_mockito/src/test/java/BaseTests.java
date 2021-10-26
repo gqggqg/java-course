5ac31b28e33ca6b6bc678946266117858fb64881
gqggqg <71404862+gqggqg@users.noreply.github.com>
2021-10-26 16:06:26 +0200

    add new tests

--- hw_03_junit_mockito/src/test/java/BaseTests.java
+++ hw_03_junit_mockito/src/test/java/BaseTests.java
@@ -9,7 +9,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collection;
 
 @RunWith(GuiceJUnitRunner.class)
 @GuiceJUnitRunner.GuiceModules(BaseTests.TestModule.class)
@@ -31,7 +30,7 @@ public class BaseTests {
     }
 
     @Test(expected = IllegalStateException.class)
-    public void bookCountIsGreaterLibraryCapacity() throws IllegalStateException {
+    public void bookCountIsGreaterLibraryCapacity() {
         libraryFactory.library(50);
     }
 
@@ -49,5 +48,44 @@ public class BaseTests {
         System.out.println(jsonBooks.size());
 
         Assert.assertArrayEquals(libraryBooks.toArray(), jsonBooks.toArray());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void takeBookFromEmptyCell() {
+        int capacity = 110;
+        libraryFactory.library(capacity).takeBook(capacity - 1);
+    }
+
+    @Test
+    public void returnOfRequestedBook() {
+        int capacity = 110;
+        ILibrary library = libraryFactory.library(capacity);
+        ArrayList<Book> libraryBooks = library.getBooks();
+
+        int index = 10;
+        Assert.assertEquals(libraryBooks.get(index), library.takeBook(index));
+    }
+
+    @Test
+    public void bookReturnsToFirstEmptyCell() {
+        int capacity = 110;
+        ILibrary library = libraryFactory.library(capacity);
+
+        int firstBookIndex = 7;
+        int secondBookIndex = 16;
+        library.takeBook(firstBookIndex);
+        Book book = library.takeBook(secondBookIndex);
+        library.addBook(book);
+
+        ArrayList<Book> libraryBooks = library.getBooks();
+
+        Assert.assertEquals(book, libraryBooks.get(firstBookIndex));
+        Assert.assertEquals(secondBookIndex, libraryBooks.indexOf(null));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void addingBookToFilledLibrary() {
+        int capacity = 100;
+        libraryFactory.library(capacity).addBook(new Book(new Author("J.R."), "Harry Potter"));
     }
 }
