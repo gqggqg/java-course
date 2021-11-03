4c50d932cff1795ef1b7688654b835a67f7986ac
gqggqg <71404862+gqggqg@users.noreply.github.com>
2021-11-03 20:35:48 +0200

    Add main function to start migration

--- hw_04_bd/build.gradle.kts
+++ hw_04_bd/build.gradle.kts
@@ -10,5 +10,7 @@ repositories {
 }
 
 dependencies {
-
+    implementation("org.flywaydb:flyway-core:8.0.1")
+    implementation("org.postgresql:postgresql:42.2.24.jre7")
+    implementation("org.jetbrains:annotations:22.0.0")
 }
---
+++ hw_04_bd/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.jetbrains.annotations.NotNull;
+import org.flywaydb.core.Flyway;
+
+public class Main {
+
+    public static final @NotNull String CONNECTION = "jdbc:postgresql://localhost:5432/";
+    public static final @NotNull String DB_NAME = "OnlineGameDB";
+    public static final @NotNull String USERNAME = "postgres";
+    public static final @NotNull String PASSWORD = "postgres";
+
+    public static void main(String[] args) {
+        final var flyway = Flyway
+                .configure()
+                .dataSource(CONNECTION + DB_NAME, USERNAME, PASSWORD)
+                .locations("db")
+                .load();
+        flyway.clean();
+        flyway.migrate();
+    }
+}
