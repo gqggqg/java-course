a9dd576b0f9df3b3f2cac4d9ed0206a75d32ce8e
gqggqg <71404862+gqggqg@users.noreply.github.com>
2021-12-01 20:55:20 +0200

    Fix database migrations

--- hw_08_jersey_jackson/init-db/src/main/resources/db/V2__populate_db.sql
+++ hw_08_jersey_jackson/init-db/src/main/resources/db/V2__populate_db.sql
@@ -1,3 +1,3 @@
-INSERT INTO db.product(id,name,manufacturer,quantity) VALUES(1,'product_1','manufacturer_1',1);
-INSERT INTO db.product(id,name,manufacturer,quantity) VALUES(2,'product_2','manufacturer_2',2);
-INSERT INTO db.product(id,name,manufacturer,quantity) VALUES(3,'product_3','manufacturer_3',3);
+INSERT INTO db.product(name,manufacturer,quantity) VALUES('product_1','manufacturer_1',1);
+INSERT INTO db.product(name,manufacturer,quantity) VALUES('product_2','manufacturer_2',2);
+INSERT INTO db.product(name,manufacturer,quantity) VALUES('product_3','manufacturer_3',3);
