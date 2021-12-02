fb2d43c8c498782c9b1c69c01ef34a0adc761029
gqggqg <71404862+gqggqg@users.noreply.github.com>
2021-12-02 02:09:33 +0200

    Create ResponseGenerator

---
+++ hw_08_jersey_jackson/rest-web-service/src/main/java/server/ResponseGenerator.java
@@ -0,0 +1,30 @@
+package server;
+
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+
+public final class ResponseGenerator {
+
+    public static Response getInternalServerErrorResponse(Object entity) {
+        return Response
+                .status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(entity)
+                .header(HttpHeaders.CACHE_CONTROL, "no-cache")
+                .build();
+    }
+
+    public static Response getOkResponse(Object entity) {
+        return Response
+                .ok(entity)
+                .header(HttpHeaders.CACHE_CONTROL, "no-cache")
+                .build();
+    }
+
+    public static Response getNotFoundResponse(Object entity) {
+        return Response
+                .status(Response.Status.NOT_FOUND)
+                .entity(entity)
+                .header(HttpHeaders.CACHE_CONTROL, "no-cache")
+                .build();
+    }
+}
