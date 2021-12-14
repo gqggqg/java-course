4acabf07a5247a987f19d5b7348aff9baf131310
gqggqg <71404862+gqggqg@users.noreply.github.com>
2021-12-14 07:42:51 +0200

    Update clan config

--- hw_09_vert.x/administrator/src/main/java/clan/Config.java
+++ hw_09_vert.x/administrator/src/main/java/clan/Config.java
@@ -2,9 +2,13 @@ package clan;
 
 public class Config {
 
-    public final static String USERS_KICK_EVENT = "clan.users.kick";
+    public final static String ROLE_USER = "user";
+    public final static String ROLE_MODERATOR = "moderator";
+
     public final static String MODERATORS_KICK_EVENT = "clan.moderators.kick";
     public final static String MODERATOR_JOIN_EVENT = "clan.moderators.join";
+    public final static String USERS_KICK_EVENT = "clan.users.kick";
+    public final static String USER_JOIN_EVENT = "clan.users.join";
     public final static String INACTIVE_CLAN_APPEARED_EVENT = "clan.admin.inactive";
 
     public final static String CLAN_MAP_NAME = "clans";
